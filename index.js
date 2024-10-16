0b9591e8e39b3e9582f3478ccbb8a133f1cca5a9
Shivam Pathak <shivampathak5042.com>
2024-10-17 02:05:21 +0530

    update

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ import Router from "./routes/route.js";
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: ["http://bloggit-client.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-); // to eliminate the cors error through by the browser
+app.use(cors()); // to eliminate the cors error through by the browser
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", Router);
@@ -23,8 +17,5 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
 Connection(USERNAME, PASSWORD);
-app.get('/',(req,res)=>{
-  res.json("hello")
-})
 
 app.listen(() => console.log(`server is running on port `));
