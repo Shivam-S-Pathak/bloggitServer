97fb120fcf4d4ffb4f78b35e210c16550cabe714
Shivam Pathak <shivampathak5042.com>
2024-10-17 14:30:42 +0530

    update bcrypt library

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,5 +1,5 @@
 import User from "../model/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import Token from "../model/token.js";
