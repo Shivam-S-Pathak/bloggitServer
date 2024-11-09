c4e1fdef1e486d50ca93540c7400b794777529ac
Shivam Pathak <shivampathak5042.com>
2024-11-09 12:45:29 +0530

    update url link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["https://bloggit-client.vercel.app"],
+    origin: ["https://bloggitstories.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
