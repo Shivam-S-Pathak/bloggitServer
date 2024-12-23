c6df96e4f7a33dac980bfbca43a21addc869cd40
Shivam Pathak <shivampathak5042.com>
2024-12-23 18:23:50 +0530

    update cors error

--- model/user.js
+++ model/user.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema=mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  recogniseBy: [
+    {
+      type: String,
+      required: false,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+  ],
+});
 
-    },
-    password:{
-        type:String,
-        required:true,
-
-    }
-})
-
-const User = mongoose.model("user",userSchema);
+const User = mongoose.model("user", userSchema);
 
 export default User;
