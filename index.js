a108e260eb91cb3da7c917fe175cb4219f55fbe8
Shivam Pathak <shivampathak5042.com>
2025-11-16 23:34:29 +0530

    cors fix

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["https://bloggitstories.vercel.app/"],
-    // origin: ["http://localhost:5173"],
-    // methods: ["POST", "GET", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    origin: "https://bloggitstories.vercel.app",
   })
 );
 // app.options("*", cors());
